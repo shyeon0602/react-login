93ee2841a593b111d3b74a9db090da833e91e110
shyeon0602 <july8560@naver.com>
2022-08-13 01:41:00 +0900

    onChange

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -20,17 +20,38 @@ function LoginForm({ Login, error }) {
         {/* 이름 */}
         <div className="form-group">
           <label htmlFor="name">Name: </label>
-          <input type="text" name="name" id="name" />
+          <input
+            type="text"
+            name="name"
+            id="name"
+            onChange={(e) => setDetails({ ...details, name: e.target.value })}
+            value={details.name}
+          />
+          {/* onChange를 통해 이벤트가 발생할 때마다 세부정보가 업데이트 됨, name의 세부정보가 value로 업데이트 됨 */}
         </div>
         {/* 아이디(이메일) */}
         <div className="form-group">
           <label htmlFor="email">ID: </label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={details.email}
+          />
         </div>
         {/* 비밀번호 */}
         <div className="form-group">
           <label htmlFor="password">password: </label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={details.password}
+          />
         </div>
         {/* 폼 전송: input type="submit" */}
         <input type="submit" value="LOGIN" />
